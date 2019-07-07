f2c0ae4511609e93087ccfc53b4b47c44151f6b2
kupriknikita <kupriknikita@gmail.com>
2019-07-07 21:04:07 +0300

    Change Bubble Color and Allignment

--- GroupedMessages/ChatMessageCell.swift
+++ GroupedMessages/ChatMessageCell.swift
@@ -13,11 +13,32 @@ class ChatMessageCell: UITableViewCell {
     let messageLabel = UILabel()
     let bubblebackgroundView = UIView()
     
+    var leadingConstraint: NSLayoutConstraint!
+    var trailingConstraint: NSLayoutConstraint!
+    
+    var chatMessage: ChatMessage! {
+        didSet {
+            bubblebackgroundView.backgroundColor = chatMessage.isIncoming ? .white : .darkGray
+            messageLabel.textColor = chatMessage.isIncoming ? .black : .white
+            messageLabel.text = chatMessage.text
+            
+            if chatMessage.isIncoming {
+                leadingConstraint.isActive = true
+                trailingConstraint.isActive = false
+            } else {
+                leadingConstraint.isActive = false
+                trailingConstraint.isActive = true
+            }
+        }
+    }
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        bubblebackgroundView.backgroundColor = .lightGray
-        bubblebackgroundView.layer.cornerRadius = 10
+        backgroundColor = .clear
+        
+        bubblebackgroundView.backgroundColor = .red
+        bubblebackgroundView.layer.cornerRadius = 12
         bubblebackgroundView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(bubblebackgroundView)
         
@@ -31,10 +52,9 @@ class ChatMessageCell: UITableViewCell {
         // MARK: Contrsraints for our label
         let constraints = [
             messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
-            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
             messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
-            messageLabel.widthAnchor.constraint(equalToConstant: 250),
-        
+            messageLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 250),
+            
             bubblebackgroundView.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: -16),
             bubblebackgroundView.leadingAnchor.constraint(equalTo: messageLabel.leadingAnchor, constant: -16),
             bubblebackgroundView.bottomAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 16),
@@ -42,6 +62,13 @@ class ChatMessageCell: UITableViewCell {
         ]
         
         NSLayoutConstraint.activate(constraints)
+        
+        leadingConstraint = messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32)
+        leadingConstraint.isActive = false
+        
+        trailingConstraint = messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32)
+        trailingConstraint.isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
--- GroupedMessages/ViewController.swift
+++ GroupedMessages/ViewController.swift
@@ -8,15 +8,28 @@
 
 import UIKit
 
+struct ChatMessage {
+    let text: String
+    let isIncoming: Bool
+}
+
 class ViewController: UITableViewController {
 
     fileprivate let cellId = "id"
     
-    let textMessages = [
-        "Here's my very first message",
-        "I'm going to message another long message that will word wrap",
-        "A beautiful activity indicator and modal alert written in Swift (originally developed for my app DoodleDoodle) Using blur effects, translucency, flat and bold design - all iOS 8 latest and greatest"
+    let chatMessages = [
+        ChatMessage(text: "Here's my first message", isIncoming: false),
+        ChatMessage(text: "Wow, dude, congratulations!", isIncoming: true),
+        ChatMessage(text: "A beautiful activity indicator and modal alert written in Swift (originally developed for my app DoodleDoodle) Using blur effects, translucency, flat and bold design - all iOS 8 latest and greatest", isIncoming: true),
+        ChatMessage(text: "WTF?!", isIncoming: false)
     ]
+    
+//    let textMessages = [
+//        "Here's my very first message",
+//        "I'm going to message another long message that will word wrap",
+//        "A beautiful activity indicator and modal alert written in Swift (originally developed for my app DoodleDoodle) Using blur effects, translucency, flat and bold design - all iOS 8 latest and greatest",
+//        "Yo, Nikita, Whaddup"
+//    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,15 +39,22 @@ class ViewController: UITableViewController {
         
         tableView.register(ChatMessageCell.self, forCellReuseIdentifier: cellId)
         tableView.separatorStyle = .none
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return textMessages.count
+        return chatMessages.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatMessageCell
-        cell.messageLabel.text = textMessages[indexPath.row]
+        let chatMessage = chatMessages[indexPath.row]
+        cell.chatMessage = chatMessage
+
         return cell
     }
 
