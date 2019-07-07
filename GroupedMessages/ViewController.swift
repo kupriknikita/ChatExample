4bf6fb2c51f38a3ea89d5c8a48919a3f0f34ff69
kupriknikita <kupriknikita@gmail.com>
2019-07-07 18:49:40 +0300

    Create project structure

---
+++ GroupedMessages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupedMessages
+//
+//  Created by Nikita Kuprik on 7/7/19.
+//  Copyright © 2019 Nikita Kuprik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
